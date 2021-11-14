2ec741a26c8bde29a8ad8fc1f9a5333e37cb2620
dshrout <dshrout@gmail.com>
2021-11-14 15:46:36 -0600

    Udated frontend with a public ip

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9681ea6e72bc4137bce0bae846d6c82-1705444367.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
