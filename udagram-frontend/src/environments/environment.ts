26ea9b3ce94423db14f3cf9d932aeec7b96c9f37
dshrout <dshrout@gmail.com>
2021-11-19 08:57:21 -0600

    Updating the public url for the reverse proxy (apiHost)

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://abc8c949e36f64c099921ef4c0303f9d-335860207.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a27dda62736754dafbc1144249c183f9-497426934.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
