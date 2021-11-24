96ddc92cae7d93a5fc84ecb2ca4ea51380ac3f23
dshrout <dshrout@gmail.com>
2021-11-24 17:27:24 -0600

    Expose public url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aaa16aa0fc7554dda8232cf71a620ce9-2054381549.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
