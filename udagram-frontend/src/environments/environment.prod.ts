5c5892c5d53e585ad27407f9692a29463fe64332
dshrout <dshrout@gmail.com>
2021-11-27 16:48:44 -0600

    here we go again

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a525205a9130e449daa6f682db578fe0-689928407.us-east-2.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
